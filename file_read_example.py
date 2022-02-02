e73b6ea729f4d5c4594904a2aac017c3493248a9
Jari Erämaa <jari@Jaris-MacBook-Pro.local>
2022-02-02 09:19:22 +0100

    file_read created and serialize lists combines all the files

--- file_read_example.py
+++ file_read_example.py
@@ -1,18 +1,65 @@
 import serialize_lists
+from math import ceil
 
-my_list = serialize_lists.read_dump("./data/HOUSE_TOWN_HOUSE.attributes")
 #for list_row in my_list:
 #    print(list_row)
 
-print(my_list[0])
+#print(my_list[0])
+my_list = serialize_lists.read_dump("./data/MERGED_LIST.NLTK")
+#my_list = serialize_lists.read_dump("./data/APARTMENT_DUPLEX.attributes")
 
-my_dict = my_list[0]
+tmp_list = my_list[14561:]
+print("=" * 100)
+print(tmp_list)
+print("=" * 100)
+all_keys = []
+#14561
+#print(tmp_list)
 
-keys = []
-keys = my_dict.keys()
-values = my_dict.values()
 
-for i in keys:
-    print(i, "/", my_dict[i])
+def _read_all_keys(tmp_list: list, total_count: int):
+    print(len(tmp_list))
+    counter = 0
+    for property_dict in tmp_list:
+        row_keys = property_dict.keys()
+        for single_key in row_keys:
+            if single_key not in all_keys:
+                counter += 1
+                all_keys.append(single_key)
+    print("counter:", counter)
+    if counter != total_count:
+        print("########## Count is not matching! #############, counter:",
+              counter, "_total_count", total_count)
 
-print("NUMBER:", len(keys))
+
+#_read_all_keys(tmp_list, len(my_list))
+#print("ALL KEYS ARE: ", all_keys)
+
+
+def read_list_in_patches() -> None:
+    my_list = serialize_lists.read_dump("./data/MERGED_LIST.NLTK")
+    total_count = len(my_list)
+    tmp_list = []
+    remaining_list = my_list
+    list_len = len(my_list)
+    size = 100  #how many list are read at one time
+    rounds = int(ceil(list_len / size))
+    for i in range(0, rounds):
+        tmp_list = remaining_list[0:size]
+        remaining_list = remaining_list[size:]
+        _read_all_keys(tmp_list, total_count)
+
+
+#read_list_in_patches()
+
+#print(all_keys)
+#print(len(all_keys))
+
+# keys = []
+# keys = my_dict.keys()
+# values = my_dict.values()
+
+# for i in keys:
+#     print(i, "/", my_dict[i])
+
+# print("NUMBER:", len(keys))
--- serialize_lists.py
+++ serialize_lists.py
@@ -1,4 +1,5 @@
 import pickle
+import os
 
 
 def write_dump(list_to_save: list, file_name: str) -> None:
@@ -20,3 +21,32 @@ def read_dump(file_name: str) -> list:
     loaded_list = pickle.load(open_file)
     open_file.close()
     return loaded_list
+
+
+def merge_files() -> list:
+    """
+    merges all the .attribute -files
+    """
+    counter = 0
+    merged_list = ['a']
+    all_files = os.listdir("./data/")
+    print("toimiiko tämä?")
+    for single_file in all_files:
+        if single_file.endswith(".attributes"):
+            print(single_file)
+            tmp_list = read_dump("./data/" + single_file)
+            merged_list = join_lists(merged_list, tmp_list)
+            counter += len(tmp_list)
+            merged_list.append(tmp_list)
+    print("Merged list length:", len(merged_list))
+    print("Total length:", counter)
+    write_dump(merged_list, "./data/MERGED_LIST.NLTK")
+
+
+def join_lists(base_list: list, list_to_add: list) -> list:
+    for row in list_to_add:
+        base_list.append(row)
+    return base_list
+
+
+#merge_files()
